b7b42d5d431867653dd9eb15c121022de4679beb
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-09-27 22:50:51 +0600

    front end part update and file upload with images

--- ecomm-backend/routes/api.php
+++ ecomm-backend/routes/api.php
@@ -3,11 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+// form usercontroller
 Route::post("signup", [UserController::class, 'signup']);
+Route::post('login', [UserController::class, 'login']);
+
+// form prodcut controoler
+Route::post("addproduct", [ProductController::class,'addProduct']);
--- ecomm-dashboard/src/component/Login.jsx
+++ ecomm-dashboard/src/component/Login.jsx
@@ -2,17 +2,60 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const navigate = useNavigate();
-
   useEffect(() => {
     if (localStorage.getItem("user-info")) {
       navigate("/add");
     }
   }, []);
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let item = { email, password };
+
+    let result = await fetch("http://localhost:8000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(item),
+    });
+    result = await result.json();
+    localStorage.setItem("user-info", JSON.stringify(result));
+    console.log(result);
+    navigate("/add");
+  };
+
   return (
-    <div>
+    <div className="col-sm-4 offset-sm-4">
       <h2>Login page</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+          placeholder="Email"
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          className="form-control"
+          placeholder="Password"
+        />
+
+        <button className="btn btn-primary mt-5" type="submit">
+          Sign Up
+        </button>
+      </form>
     </div>
   );
 }
